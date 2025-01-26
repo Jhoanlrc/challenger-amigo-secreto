7c866b82edf4694207aafef9fa64980818006827
Johan Rivera <johanrivera325@gmail.com>
2025-01-26 18:34:57 -0300

    Crear función agregarAmigo en app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+let listaNombres = [];
+
+function agregarAmigo() {
+    let nombreUsuario = document.getElementById("amigo").value;
+    console.log(`nombre ingresado : ${nombreUsuario}`);
+
+    // Si el nombre ingresado por el usuario está vacío
+    if (nombreUsuario == "") {
+        alert("Por favor, inserte un nombre");
+        return;
+    } else {
+        listaNombres.push(nombreUsuario);
+        console.log("lista nombres tiene  " + listaNombres.length + (listaNombres.length == 1 ? " nombre" : " nombres"));       
+        // Limpiar cajita
+        document.getElementById("amigo").value = "";
+    }
+    
+}
+
+
